cdc356241b421a16082cc7b6b857d19bb2c79fa2
潘晶 <1583030729@qq.com>
2019-03-05 13:08:44 +0800

    这是利用java.util.Arrays.sort来进行排序，然后输出最小值

---
+++ arrayMinElement.java
@@ -0,0 +1,12 @@
+public class arrayMinElement{
+	
+	public static void main(String[] args){
+		
+		int[] array = {29,50,35,73,32,9999,109};
+		
+		java.util.Arrays.sort(array);
+		
+		System.out.println(array[0]);
+		
+	}
+}	
