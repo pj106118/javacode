21083747050d012bab5ef706e51122b2b96dd59b
潘晶 <1583030729@qq.com>
2019-03-06 16:27:27 +0800

    这是通过给定的数据来计算圆柱体积的java程序

---
+++ CalculationVolume.java
@@ -0,0 +1,22 @@
+
+public class CalculationVolume{
+	
+	public static void main(String[] args){
+		
+		double radius = 5.5;
+		
+		double length = 12;
+		
+		double num = 3.1415926;
+		
+		double area = radius * radius * num;
+		
+		double volume = area * length;
+		
+		System.out.println("The area is " + area);
+		
+		System.out.println("The Volume is " + volume);
+		
+	}
+	
+}
