a7c518e5f2a116320e12e21d8142795188c001a0
潘晶 <1583030729@qq.com>
2019-03-28 16:50:58 +0800

    使用循环打印图案

---
+++ PrintlnTable.java
@@ -0,0 +1,12 @@
+package jing.able;
+
+public class PrintlnTable {
+    public static void main(String[] args){
+        for(int i = 1;i<7;i++){
+            for(int j= 1;j<=7-i;j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
