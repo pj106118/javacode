66a9c3105fffdcde2ad9bbc86fec1707d3e397cd
潘晶 <1583030729@qq.com>
2019-04-16 21:24:46 +0800

    简单队列的实现

---
+++ IMyQueue.java
@@ -0,0 +1,21 @@
+package jing.able.impl;
+
+/**
+ * @author: panjing
+ * @describe:
+ * @date: 2019/4/14
+ * @time: 15:37
+ */
+public interface IMyQueue {
+    // 判断这个队列是否为空
+    boolean empty();
+    // 返回队首元素，但不出队列
+    int peek();
+    // 返回队首元素，并且出队列
+    int poll();
+    // 将 item 放入队列中
+    void add(int item);
+    // 返回元素个数
+    int size();
+
+}
