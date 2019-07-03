5746df30d58c47faf67f0c0609eeb8935aec38de
潘晶 <1583030729@qq.com>
2019-07-03 18:07:29 +0800

    Find the sum of the digits of the input and the sum of the digits of its square

---
+++ AddTest.java
@@ -0,0 +1,31 @@
+package jing.able;
+
+/**
+ * @author : panjing
+ * @data : 2019/7/3 9:53
+ * @describe :  数字之和
+ */
+import java.util.Scanner;
+
+public class AddTest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            int num = sc.nextInt();
+            int a = add(num);
+            int b = add(num*num);
+            System.out.println(a +" " + b);
+        }
+        sc.close();
+    }
+    public static int add(int num){
+        int count = 0;
+        while(num!=0){
+            count += num % 10; //余数
+            num /= 10;
+        }
+        return count;
+    }
+
+
+}
