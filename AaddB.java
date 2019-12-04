4377d75e7d20cd89c0fcbd82ff6ac5f1ae89916f
潘晶 <1583030729@qq.com>
2019-12-04 20:57:02 +0800

    There are two integers represented by linked lists, each node containing a digit.The digits are stored in reverse, so the ones are at the top of the list.Write a function that sums these two integers and returns the result as a linked list.

---
+++ AaddB.java
@@ -0,0 +1,49 @@
+package jing.able.OJ;
+
+/**
+ * @author : panjing
+ * @data : 2019/12/1 20:55
+ * @describe :  链式A+B
+ * 有两个用链表表示的整数，每个结点包含一个数位。这些数位是反向存放的，也就是个位排在链表的首部。
+ * 编写函数对这两个整数求和，并用链表形式返回结果。
+ */
+
+public class AaddB {
+    public class ListNode {
+        int val;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+        public ListNode plusAB(ListNode a, ListNode b) {
+            // write code here
+            ListNode list = new ListNode(-1);
+            ListNode head = list;
+            ListNode p = a;
+            ListNode q = b;
+            int sum = 0;
+            if(a == null){
+                return b;
+            }
+            if(b==null){
+                return a;
+            }
+            while(p != null || q != null || sum !=0){
+                if(p!=null){
+                    sum += p.val;
+                    p = p.next;
+                }
+                if(q!=null){
+                    sum += q.val;
+                    q = q.next;
+                }
+                head.next = new ListNode(sum%10);
+                sum /= 10;
+                head = head.next;
+            }
+
+            return list.next;
+        }
+}
