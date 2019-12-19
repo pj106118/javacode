ae72fe7d485788130ecc7df3ceabb5919771cde6
潘晶 <1583030729@qq.com>
2019-12-19 20:28:23 +0800

    Implement queues with stacks

---
+++ StackstoQueues.java
@@ -0,0 +1,22 @@
+//用栈实现队列
+import java.util.Stack;
+
+public class StackstoQueues {
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+    
+    public void push(int node) {
+            stack1.push(node);
+    }
+    
+    public int pop() {
+        while(!stack1.isEmpty()){
+            stack2.push(stack1.pop());
+            }
+        int solve = stack2.pop();
+        while(!stack2.isEmpty()){
+            stack1.push(stack2.pop());
+        }
+        return solve;
+    }
+}
