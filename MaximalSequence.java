2fd5ec0f179bbc44a8fb3aad747da752a2e02813
潘晶 <1583030729@qq.com>
2020-01-03 14:32:20 +0800

    HZ occasionally tries to trick non-computer majors with professional questions.After today's test group meeting, he added: in the old one-dimensional pattern recognition, it was often necessary to calculate the maximum sum of the continuous subvectors, and when all the vectors were positive, the problem was easy to solve.But if you have negative Numbers in your vector, should you include some negative number and expect the positive Numbers next to it to make up for it? For example: {6-3-2, 7, 15,1,2,2}, the largest and continuous vector son is 8 (starting from 0, until the third). Give an array, return the sum of its maximum continuous subsequences, and you won't be fooled by it?(the length of the subvector is at least 1)

---
+++ MaximalSequence.java
@@ -0,0 +1,34 @@
+package jing.able.OJ;
+
+import java.nio.charset.MalformedInputException;
+
+/**
+ * @author : panjing
+ * @data : 2020/1/1 16:57
+ * @describe : 最长子序列
+ *
+ * HZ偶尔会拿些专业问题来忽悠那些非计算机专业的同学。今天测试组开完会后,他又发话了:
+ * 在古老的一维模式识别中,常常需要计算连续子向量的最大和,当向量全为正数的时候,问题很好解决。
+ * 但是,如果向量中包含负数,是否应该包含某个负数,并期望旁边的正数会弥补它呢？
+ * 例如:{6,-3,-2,7,-15,1,2,2},连续子向量的最大和为8(从第0个开始,到第3个为止)。
+ * 给一个数组，返回它的最大连续子序列的和，你会不会被他忽悠住？(子向量的长度至少是1)
+ */
+public class MaximalSequence {
+
+    public static void main(String[] args) {
+
+        int[] array = {-2,-8,-1,-5,-9};
+        int max = Integer.MIN_VALUE;
+        for(int i = 0;i<array.length;i++){
+            int sum = 0;
+            for(int j = i;j<array.length;j++){
+                sum+=array[j];
+                if(sum>max){
+                    max = sum;
+                }
+            }
+        }
+
+        System.out.println(max);
+    }
+}
