2008b59bb8aacb8f7f39888a64a53a020e12bc3d
潘晶 <1583030729@qq.com>
2020-01-25 22:45:50 +0800

    Define an abstract "Role" with name, age, gender, and other member variables

--- AbstractRole.java
+++ AbstractRole.java
@@ -1,20 +1,16 @@
 package jing.able;
 
 /**
- * @author: panjing
- * @describe: 定义一个抽象的"Role"有姓名，年龄，性別等成员变量
- * 1)要求尽可能隐藏所有変量(能够私有就私有，能够保护就不要公有)，
- * 再通过Getter()和Setter()方法対各变量进行读写。具有一个抽象的play()方法、
- * 该方法不返回任何值，同时至少定义两个构造方法。Role类中要体现出this的几种用法。
- * 2)从Role类派生出一个"Employee"类，该类具有Role类的所有成员(构造方法除外)
- * 并扩展salary成员变量,同时増加一个静态成员变量“职工编号(ID) "。
- * 同样要有至少两个构造方法，要体现出this和super的几种用法,还要求覆盖play()方法，
- * 并提供一个final sing()方法。
- * 3)"Manager"类继承"Employee"类.有一个final成员变量" vehicle"
- * 在main()方法中制造Manager和Employe对象,并测试这些对象的方法。
- * @date: 2019/4/25
- * @time: 13:24
- */
+ 定义一个抽象的"Role"有姓名，年龄，性別等成员变量
+ 1)要求尽可能隐藏所有変量(能够私有就私有，能够保护就不要公有)，
+ 再通过Getter()和Setter()方法対各变量进行读写。具有一个抽象的play()方法、* 该方法不返回任何值，同时至少定义两个构造方法。Role类中要体现出this的几种用法。
+  2)从Role类派生出一个"Employee"类，该类具有Role类的所有成员(构造方法除外)
+  并扩展salary成员变量,同时増加一个静态成员变量“职工编号(ID) "。
+  同样要有至少两个构造方法，要体现出this和super的几种用法,还要求覆盖play()方法，
+  并提供一个final sing()方法。
+  3)"Manager"类继承"Employee"类.有一个final成员变量" vehicle"
+  在main()方法中制造Manager和Employe对象,并测试这些对象的方法。
+*/
 
 public class AbstractRole {
     public static void main(String[] args) {
