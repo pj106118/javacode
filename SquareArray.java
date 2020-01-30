6f5ea65a47252e81671e26b939e21d1d3bd86f0b
潘晶 <1583030729@qq.com>
2020-01-30 22:47:10 +0800

    Given an array of integers A sorted in non-decreasing order, A new array consisting of the squares of each number is returned to be sorted in non-decreasing order as well.

---
+++ SquareArray.java
@@ -0,0 +1,14 @@
+/*
+给定一个按非递减顺序排序的整数数组 A，返回每个数字的平方组成的新数组，要求也按非递减顺序排序。
+*/
+import java.util.*;
+class SquareArray  {
+    public int[] sortedSquares(int[] A) {
+        int[] B = new int[A.length];
+        for(int i =0;i<A.length;i++){
+            B[i] = A[i] *A[i];
+        }
+        Arrays.sort(B);
+        return B;
+    }
+}
