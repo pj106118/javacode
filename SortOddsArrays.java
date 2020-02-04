d7b936e83d9d36827c6a77c8b8d155479c1fd8c5
潘晶 <1583030729@qq.com>
2020-02-04 21:42:33 +0800

    Given an array of non-negative integers, A, returns an array in which all the even elements of A are followed (in ascending order) by all the odd elements.

--- SortOddsArrays.java
+++ SortOddsArrays.java
@@ -1,9 +1,9 @@
 /*
-给定一个非负整数数组 A，返回一个数组，在该数组中， A 的所有偶数元素之后跟着所有奇数元素。
+给定一个非负整数数组 A，返回一个数组，在该数组中， A 的所有偶数元素之后跟着所有奇数元素(按升序的方式)。
 */
 class SortOddsArrays {
     public int[] sortArrayByParity(int[] A) {
-        
+        Arrays.sort(A);
         int [] a = new int[A.length];
         int right = A.length-1;
         int left = 0;
